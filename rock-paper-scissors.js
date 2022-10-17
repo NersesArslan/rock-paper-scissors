112fea40482fd1115c5a89fcbf6ff11e49587ecd
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-10-17 20:45:07 +0300

    Add rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,28 @@
 /* function that returns Rock, Paper or Scissors */
 
+let userInput = prompt("Rock, Paper or Scissors?");
 
-function getComputerChoice(rps) 
+function getComputerChoice() 
     {
+        const rps = ["Rock", "Paper", "Scissors"];
     return rps[Math.floor(Math.random()*rps.length)];
     }
-const rps = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(rps));
 
-function playRound(playerSelection, computerSelection) {
-    switch(playerSelection && computerSelection){
-        case playerSelection === "Rock" && computerSelection === "Scissors":
-        return "You win! Rock beats Scissors!";
-        
+let computerSelection = getComputerChoice();
+let playerSelection = userInput;
+
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection ==="Rock"){
+        if(computerSelection === "Paper"){
+            return "You lose! Paper covers Rock!"
+        }
+        else if(computerSelection === "Scissors"){
+            return "You win! Rock beats Scissors!";
+        } else {
+            return "Draw.";
+        }
     }
-}
+};
+   
