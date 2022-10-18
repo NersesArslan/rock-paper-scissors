8a5a994b84dcdc0b644625f368e46ecbda7e485a
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-10-18 07:19:43 +0300

    Add rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,36 +8,43 @@ function getComputerChoice()
     return rps[Math.floor(Math.random()*rps.length)];
     }
 
+const playerSelection = userInput;
+const computerSelection = getComputerChoice();
+
 function playRound(playerSelection, computerSelection){
-    let playerSelection = playerSelection.toLowerCase();
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
     if(playerSelection === computerSelection){
         return "Draw";
     } 
-    else if (playerSelection ==="rock"){
-        if(computerSelection === "Paper"){
+    else if (playerSelection === "rock"){
+        if(computerSelection === "paper"){
             return "You lose! Paper covers Rock."
         }
-        else if (computerSelection === "Scissors"){
+        else if (computerSelection === "scissors"){
             return "You win! Rock beats Scissors!";
         } 
     }
     else if(playerSelection === "paper"){
-        if(computerSelection === "Scissors"){
+        if(computerSelection === "scissors"){
             return "You lose! Scissors cuts Paper.";
         } 
-        else if(computerSelection === "Rock"){
+        else if(computerSelection === "rock"){
             return "You win! Paper covers Rock.";
         }
     }
     else if(playerSelection === "scissors"){
-        if(computerSelection === "Rock"){
+        if(computerSelection === "rock"){
             return "You lose! Rock beats Scissors.";
         }
-    else if(computerSelection ==="Paper"){
+    else if(computerSelection ==="paper"){
         return "You win! Scissors cuts Paper.";
     }
     }
 };
 
-console.log(playRound(userInput, getComputerChoice()));
+
+console.log(playRound(playerSelection, computerSelection));
    
