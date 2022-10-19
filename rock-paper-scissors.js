eceaedcee1e076008e734e02f72a92e256fedcda
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-10-19 19:10:44 +0300

    Add rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,30 +13,30 @@ function playRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
-        return "Draw";
+        return 0;
     } 
     else if (playerSelection === "rock"){
         if(computerSelection === "paper"){
-            return "You lose!";
+            return -1;
         }
         else if (computerSelection === "scissors"){
-            return "You win!";
+            return 1;
         } 
     }
     else if(playerSelection === "paper"){
         if(computerSelection === "scissors"){
-            return "You lose!";
+            return -1;
         } 
         else if(computerSelection === "rock"){
-            return "You win!";
+            return 1;
         }
     }
     else if(playerSelection === "scissors"){
         if(computerSelection === "rock"){
-            return "You lose!";
+            return -1;
         }
     else if(computerSelection === "paper"){
-        return "You win!";
+        return 1;
     }
     }
 };
@@ -46,16 +46,34 @@ function playRound(playerSelection, computerSelection){
 function game(){
     let playerScore = 0;
     let computerScore = 0;
-    
+    let score = 0;
     for(let i = 0; i < 5; i++){      
-        
-
         const playerSelection = prompt("Rock, Paper or Scissors?"); 
         const computerSelection = getComputerChoice();    
-        console.log(playRound(playerSelection, computerSelection));
+        if(playRound(playerSelection, computerSelection) === 0){
+            console.log("Draw");
+        }
+        else if(playRound(playerSelection, computerSelection) === 1){
+            console.log("You win this round!");
+        }
+        else if(playRound(playerSelection, computerSelection) === -1){
+            console.log("You lose this round!");
         
+        }
+        
+        score += score;
        }
+       if(score > 0){
+        console.log(alert("You Win!"));
+       }
+       else if(score < 0){
+        console.log(alert("Computer Wins!"));
+       }
+       else if(score === 0){
+        console.log(alert("Tie Game"));
+       }
+    
     } 
     
-}
+
 
