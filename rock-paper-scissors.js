1df12b175677fcde1511180cc5abf4803caca100
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-10-25 20:39:00 +0300

    adding the UI to the RPS project

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,5 @@
-game();
+
+
 
 function getComputerChoice() 
     {
@@ -6,74 +7,71 @@ function getComputerChoice()
     return rps[Math.floor(Math.random()*rps.length)];
     }
 
-
+    const computerSelection = getComputerChoice(); 
+    
 function playRound(playerSelection, computerSelection){
 
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
-        return 0;
+        return "Draw";
     } 
     else if (playerSelection === "rock"){
         if(computerSelection === "paper"){
-            return -1;
+            return "You lose!";
+            
         }
         else if (computerSelection === "scissors"){
-            return 1;
+            return "You win!";
         } 
     }
     else if(playerSelection === "paper"){
         if(computerSelection === "scissors"){
-            return -1;
+            return "You lose!";
         } 
         else if(computerSelection === "rock"){
-            return 1;
+            return "You win!";
         }
     }
     else if(playerSelection === "scissors"){
         if(computerSelection === "rock"){
-            return -1;
+            return "You lose!";
         }
     else if(computerSelection === "paper"){
-        return 1;
+        return "You win!";
     }
     }
 };
 
 
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let score = 0;
-    for(let i = 0; i < 5; i++){      
-        const playerSelection = prompt("Rock, Paper or Scissors?"); 
-        const computerSelection = getComputerChoice();    
-        if(playRound(playerSelection, computerSelection) === 0){
-            console.log("Draw");
-        }
-        else if(playRound(playerSelection, computerSelection) === 1){
-            console.log("You win this round!");
-        }
-        else if(playRound(playerSelection, computerSelection) === -1){
-            console.log("You lose this round!");
-        
-        }
-        
-        score += score;
-       }
-       if(score > 0){
-        console.log(alert("You Win!"));
-       }
-       else if(score < 0){
-        console.log(alert("Computer Wins!"));
-       }
-       else if(score === 0){
-        console.log(alert("Tie Game"));
-       }
-    
-    } 
+const Rock = document.querySelector('.Rock');
+const Paper = document.querySelector('.Paper');
+const Scissors = document.querySelector('.Scissors');
+
+Rock.addEventListener('click', () => {
+     playerSelection = "rock";
+     alert(playRound(playerSelection, computerSelection));
+     console.log(playerSelection);
+     console.log(computerSelection);
+})
+
+Paper.addEventListener('click', () => {
+    playerSelection = "paper";
+    alert(playRound(playerSelection, computerSelection));
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+})
+
+Scissors.addEventListener('click', () => {
+    playerSelection = "scissors";
+    alert(playRound(playerSelection, computerSelection));
+    console.log(playerSelection);
+    console.log(computerSelection);
+})
+
     
 
 
